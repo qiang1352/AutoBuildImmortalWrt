e1c572650fcc7a874fe133cae076f5818a88fd47
qiang1352 <79758189+qiang1352@users.noreply.github.com>
2025-10-21 22:38:29 +0800

    Remove luci-i18n-passwall-zh-cn from build script
    Removed luci-i18n-passwall-zh-cn from PACKAGES.

--- x86-64/build.sh
+++ x86-64/build.sh
@@ -46,7 +46,6 @@ PACKAGES="$PACKAGES luci-i18n-upnp-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-package-manager-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
-PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
 # 文件管理器
 PACKAGES="$PACKAGES luci-i18n-filemanager-zh-cn"
 # 静态文件服务器dufs(推荐)
